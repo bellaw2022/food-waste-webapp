42b54ad0872317f329010581ade943b3bfcb683f
David Chi <davidchi31415@gmail.com>
2024-10-26 15:34:49 -0500

    Added back oauth logic, but localhost does not work.

---
+++ fridgify-frontend/src/pages/home/index.tsx
@@ -0,0 +1,20 @@
+export default function Home({ profile, login, logOut }) {
+    return (
+        <div>
+            <h2>Welcome to Fridgify!</h2>
+            <br />
+            {profile ? (
+                <div>
+                    <img src={profile.picture} alt="User" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google </button>
+            )}
+        </div>
+    );
+}
