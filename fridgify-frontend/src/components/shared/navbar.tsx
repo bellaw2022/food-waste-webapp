55c740efc876a8f778ecd0d666ee110e5654a8b2
David Chi <davidchi31415@gmail.com>
2024-10-31 19:00:57 -0500

    Implemented layouts.

---
+++ fridgify-frontend/src/components/shared/navbar.tsx
@@ -0,0 +1,34 @@
+import { PlusCircleIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { Link, useLocation } from "react-router-dom";
+
+const buttonRoutes = [
+    { name: "Home", path: "/" },
+    { name: "Inventory", path: "/inventory" },
+    { name: "Recipe Rec", path: "/recipe" },
+]
+
+export const Navbar = () => {
+    const location = useLocation();
+
+    if (location.pathname === "/scan") return null;
+
+    return (
+        <div className="m-3 flex flex-row items-center justify-between">
+            <Link to="/scan">
+                <Button className="p-2 h-fit w-fit rounded-full bg-[green]" variant="outline">
+                    <PlusCircleIcon size={32} color="white" />
+                </Button>
+            </Link>
+            <div className="flex flex-row items-center justify-center gap-1">
+                {buttonRoutes.map((route) => (
+                    <Link to={route.path}>
+                        <Button variant="outline">
+                            {route.name}
+                        </Button>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
