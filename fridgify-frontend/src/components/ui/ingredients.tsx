54eb40a0d9dc5590272cb92d2c65f72ddafc55f3
yuqian-cao-19 <caoyuqian520@gmail.com>
2024-11-08 10:50:34 -0600

    added search bar

--- fridgify-frontend/src/components/ui/ingredients.tsx
+++ fridgify-frontend/src/components/ui/ingredients.tsx
@@ -10,10 +10,12 @@ const Ingredients: React.FC<IngredientsProps> = ({
   ingredients,
   onSelectionChange,
 }) => {
+  // keep track of selected ingredients
   const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
-
+  // whether or not to display all ingredients or just the first 10
   const [showAll, setShowAll] = useState(false);
-
+  // user input in search bar
+  const [searchQuery, setSearchQuery] = useState("");
   const handleIngredientClick = (ingredient: string) => {
     const newSelectedIngredients = selectedIngredients.includes(ingredient)
       ? selectedIngredients.filter((i) => i !== ingredient)
@@ -25,26 +27,40 @@ const Ingredients: React.FC<IngredientsProps> = ({
 
   const toggleShowAll = () => setShowAll(!showAll);
 
+  // Filter ingredients based on the search query
+  const filteredIngredients = ingredients.filter((ingredient) =>
+    ingredient.toLowerCase().startsWith(searchQuery.toLowerCase())
+  );
+
   return (
-    <div className="ingredients">
-      {ingredients
-        .slice(0, showAll ? ingredients.length : 10)
-        .map((ingredient, index) => (
-          <button
-            className={`ingredient-button ${
-              selectedIngredients.includes(ingredient) ? "selected" : ""
-            }`}
-            key={index}
-            onClick={() => handleIngredientClick(ingredient)}
-          >
-            {ingredient}
+    <div>
+      <input
+        type="text"
+        placeholder="Search ingredients..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="search-bar"
+      ></input>
+      <div className="ingredients">
+        {filteredIngredients
+          .slice(0, showAll ? ingredients.length : 10)
+          .map((ingredient, index) => (
+            <button
+              className={`ingredient-button ${
+                selectedIngredients.includes(ingredient) ? "selected" : ""
+              }`}
+              key={index}
+              onClick={() => handleIngredientClick(ingredient)}
+            >
+              {ingredient}
+            </button>
+          ))}
+        {filteredIngredients.length > 10 && (
+          <button onClick={toggleShowAll} className="ingredient-button">
+            {showAll ? "▲" : "... ▼"}
           </button>
-        ))}
-      {ingredients.length > 10 && (
-        <button onClick={toggleShowAll} className="ingredient-button">
-          {showAll ? "▲" : "... ▼"}
-        </button>
-      )}
+        )}
+      </div>
     </div>
   );
 };
