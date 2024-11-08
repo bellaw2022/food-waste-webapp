fb8456b180629989a1aa2b9317f80364da268939
yuqian-cao-19 <caoyuqian520@gmail.com>
2024-11-08 10:36:17 -0600

    display ingredients in correct format

--- fridgify-frontend/src/pages/recipe/index.tsx
+++ fridgify-frontend/src/pages/recipe/index.tsx
@@ -1,7 +1,36 @@
+import Ingredients from "@/components/ui/ingredients";
+
 export const RecipePage = () => {
-    return (
-        <div className="mx-4">
-            <h1 className="text-3xl font-bold">Recipe Rec</h1>
-        </div>
-    );
-}
+  const ingredients = [
+    "Tomato",
+    "Lettuce",
+    "Cheese",
+    "Bacon",
+    "Onion",
+    "Pickle",
+    "Ketchup",
+    "Mayo",
+    "Mustard",
+    "Relish",
+    "Cucumber",
+    "Carrot",
+    "Radish",
+    "Olive",
+    "Pepper",
+    "Garlic",
+  ];
+
+  const handleSelectionChange = (selectedIngredients: string[]) => {
+    console.log("Selected ingredients:", selectedIngredients);
+  };
+
+  return (
+    <div className="mx-4">
+      <h1 className="text-3xl font-bold">Recipe Rec</h1>
+      <Ingredients
+        ingredients={ingredients}
+        onSelectionChange={handleSelectionChange}
+      ></Ingredients>
+    </div>
+  );
+};
