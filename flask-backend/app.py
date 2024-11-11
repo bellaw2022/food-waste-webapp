746150b5464361a3fd87914fd58bd12ebb091d35
yuqian-cao-19 <caoyuqian520@gmail.com>
2024-11-11 14:35:44 -0600

    added api fetching functions to recipes_routes

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -6,6 +6,8 @@ from config import Config
 from models import db
 from routes.user_routes import user_routes
 from routes.userwastesaving_routes import userwastesaving_routes
+from routes.user_produce_routes import user_produce_routes
+from routes.recipes_routes import recipes_routes
 
 app = Flask(__name__)
 
@@ -18,6 +20,8 @@ migrate = Migrate(app, db)
 # routes
 app.register_blueprint(user_routes)
 app.register_blueprint(userwastesaving_routes)
+app.register_blueprint(user_produce_routes)
+app.register_blueprint(recipes_routes)
 
 
 @app.route('/')
