c103e7e62f4482d6a409aa0f85fb1b6e494f5236
David Chi <davidchi31415@gmail.com>
2024-11-11 16:54:55 -0600

    Implemented api requests for produce.

--- fridgify-frontend/src/App.tsx
+++ fridgify-frontend/src/App.tsx
@@ -1,26 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClientProvider } from "@tanstack/react-query";
 import { HomePage, BeginScanningPage, FinishScanningPage, InventoryPage, RecipePage, ProfilePage} from '@/pages';
 import { useOAuth } from "@/hooks";
 import { Navbar } from '@/components/shared';
 import { Toaster } from '@/components/ui/sonner';
+import APIClient from "@/api";
 
 function App() {
     const { profile, login, logOut } = useOAuth();
     return (
-        <Router>
-            <div className="m-0 p-0">
-                <Navbar/>
-                <Routes>
-                    <Route path="/" element={<HomePage profile={profile} login={login} logOut={logOut} />} />
-                    <Route path="/inventory" element={<InventoryPage />} />
-                    <Route path="/recipe" element={<RecipePage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                    <Route path="/scan" element={<BeginScanningPage />} />
-                    <Route path="/scan/finish" element={<FinishScanningPage />} />
-                </Routes>
-                <Toaster />
-            </div>
-        </Router>
+        <QueryClientProvider client={APIClient}>
+            <Router>
+                <div className="m-0 p-0">
+                    <Navbar/>
+                    <Routes>
+                        <Route path="/" element={<HomePage profile={profile} login={login} logOut={logOut} />} />
+                        <Route path="/inventory" element={<InventoryPage />} />
+                        <Route path="/recipe" element={<RecipePage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route path="/scan" element={<BeginScanningPage />} />
+                        <Route path="/scan/finish" element={<FinishScanningPage />} />
+                    </Routes>
+                    <Toaster />
+                </div>
+            </Router>
+        </QueryClientProvider>
     )
 }
 
---
+++ fridgify-frontend/src/api/produce-catalog/index.ts
@@ -0,0 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "@/api/constants";
+import { CatalogItem } from "@/store";
+
+export const useProduceCatalog = () => {
+    const query = useQuery({
+        queryKey: ["produce-catalog"],
+        queryFn: async () => {
+            const response1 = await fetch(`${API_URL}/all_produces`);
+            if (response1.status !== 200) {
+              throw new Error('Could not fetch names of produce catalog items!');
+            }
+
+            const data1 = await response1.json();
+            const names = data1?.data;
+            if (!names) {
+                throw new Error("Could not get names from backend response!");
+            }
+            
+            const response2 = await fetch(`${API_URL}/produce`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    produces: names,
+                }),
+            });
+            if (response2.status !== 200) {
+                throw new Error("Failed to get info about produce catalog items!");
+            }
+
+            const data2 = await response2.json();
+
+            const result = data2.data;
+            Object.keys(result).forEach((name) => {
+                result[name] = {
+                    category: result[name].category ?? undefined,
+                    unit: result[name].unit,
+                    expirationDays: result[name].common_expdate,
+                    productId: result[name].product_id,
+                }
+            });
+
+            return result as Record<string, CatalogItem>;
+        }
+    });
+
+    return {
+        isCatalogLoading: query.isFetching,
+        isCatalogError: query.isError,
+        produceCatalog: query.data,
+    }
+};
