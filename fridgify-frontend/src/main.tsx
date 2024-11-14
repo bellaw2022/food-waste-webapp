7ff51d9bc5803780c42766a05f033b9286a121b4
yuqian-cao-19 <caoyuqian520@gmail.com>
2024-11-14 14:09:57 -0600

    used context for global user id

--- fridgify-frontend/src/main.tsx
+++ fridgify-frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App.tsx";
+import "./index.css";
+import { AppProvider } from "./AppContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="647856339439-ch1qle9m7uqdu41f5fsen6soqd019cua.apps.googleusercontent.com">
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
