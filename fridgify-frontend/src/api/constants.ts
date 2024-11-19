17f0f9fffb85bc8d04d242aa15b187f897a8f2b8
bellaw2022 <bellaw2022@tamu.edu>
2024-11-19 13:38:08 -0600

    removed localhost for testing

--- fridgify-frontend/src/api/constants.ts
+++ fridgify-frontend/src/api/constants.ts
@@ -1,3 +1,3 @@
-const BACKEND_URL = process.env.VITE_BACKEND_URL ?? "http://localhost:10000";
-
+// const BACKEND_URL = process.env.VITE_BACKEND_URL ?? "http://localhost:10000";
+const BACKEND_URL = process.env.VITE_BACKEND_URL;
 export const API_URL = `${BACKEND_URL}/api`;
